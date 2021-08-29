dbf5d997c10ddfe7f847cf9d2e395bc53c015e05
jmonarch-gofinalmile <89747350+jmonarch-gofinalmile@users.noreply.github.com>
2021-08-29 14:31:01 -0500

    Changed Build Style
    Changed build style to EXE to deploy more easily

--- Dev/DesktopCentralInstall/InstallDC.ps1
+++ Dev/DesktopCentralInstall/InstallDC.ps1
@@ -11,23 +11,20 @@
 		Installs Desktop Central on client workstations
 #>
 
-$downjson = 'https://gofinalmile.sharepoint.com/:u:/s/ITBI/EX2_3QnbXmBJiYEqXoak_3MBHYl4P3rHNAFZRgMpYpCCEA?e=IZcz3v'
-$downmsi = 'https://gofinalmile-my.sharepoint.com/:u:/p/jmonarch/EbvdtLoibShEsCiWP74I_QMBTUMVmS_fuMiIb5ZLEocKXg?e=XesiH9'
-$downmst = 'https://gofinalmile.sharepoint.com/:u:/s/ITBI/ESqP4kk1wgNCkECPWIX6MKQBq-BaeDU2QcV-bD02NaZaOg?e=py8Dc9'
+$downjson = 'https://github.com/jmonarch-gofinalmile/Scripts/raw/main/Dev/DesktopCentralInstall/DCAgentServerInfo.json'
+$downmsi = 'https://github.com/jmonarch-gofinalmile/Scripts/raw/main/Dev/DesktopCentralInstall/DesktopCentralAgent.msi'
+$downmst = 'https://github.com/jmonarch-gofinalmile/Scripts/raw/main/Dev/DesktopCentralInstall/DesktopCentralAgent.mst'
 
 Invoke-WebRequest -Uri $downjson -OutFile "C:\Windows\Temp\DCAgentServerInfo.json"
 Invoke-WebRequest -Uri $downmsi -OutFile "C:\Windows\Temp\DesktopCentralAgent.msi"
 Invoke-WebRequest -Uri $downmst -OutFile "C:\Windows\Temp\DesktopCentralAgent.mst"
 
-## Get Running Directory
-#$dirraw = Get-Location
-#$dir = $dirraw.Path
-#
-#$msi = 'DesktopCentralAgent.msi'
-#$mst = 'DesktopCentralAgent.mst'
-#
-#$msipath = $dir + '\' + $msi
-#$mstpath = $dir + '\' + $mst
-#
-#Start-Process msiexec.exe -ArgumentList "/i $msipath /qn TRANSFORMS=$mstpath /lv %temp%\Agentinstalllog.txt" -Wait
+$dir = 'C:\Windows\Temp'
+$msi = 'DesktopCentralAgent.msi'
+$mst = 'DesktopCentralAgent.mst'
+
+$msipath = $dir + '\' + $msi
+$mstpath = $dir + '\' + $mst
+
+Start-Process msiexec.exe -ArgumentList "/i $msipath /qn TRANSFORMS=$mstpath /lv C:\Windows\Temp\Agentinstalllog.txt" -Wait
 
